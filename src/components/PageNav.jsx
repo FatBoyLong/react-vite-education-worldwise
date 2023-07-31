951ca07e72689abba64ade3fc1c0bc1df36426f1
FatBoyLong <eugggggene@gmail.com>
2023-07-31 21:27:41 +0300

    added premade components

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,9 +1,11 @@
 import { NavLink } from 'react-router-dom';
 // NavLink allows us to highlight currently chosen page. Adding class 'active' to current link element
 
+import styles from './PageNav.module.css';
+
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <NavLink to="/">Homepage</NavLink>
