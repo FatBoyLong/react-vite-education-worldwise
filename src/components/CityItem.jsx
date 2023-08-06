e63910d7b4d9230c542018992447dcd818c23d60
FatBoyLong <eugggggene@gmail.com>
2023-08-06 18:04:52 +0300

    implemented nested routes and list components

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,25 @@
+import styles from './CityItem.module.css';
+
+function CityItem({ city }) {
+  console.log(city);
+  const { cityName, country, date, emoji, id, notes, position } = city;
+
+  // Date formatter function
+  const formatDate = (date) =>
+    new Intl.DateTimeFormat('en', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    }).format(new Date(date));
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>({formatDate(date)})</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
+}
+
+export default CityItem;
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,20 +1,26 @@
 import { NavLink } from 'react-router-dom';
 // NavLink allows us to highlight currently chosen page. Adding class 'active' to current link element
 
+import Logo from './Logo';
+
 import styles from './PageNav.module.css';
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
-        <li>
-          <NavLink to="/">Homepage</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
